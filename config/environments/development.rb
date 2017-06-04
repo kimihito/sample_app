40de428cd10e343d8b25e941830d696a0ab5febe
kimihito <tatsurotamashiro@gmail.com>
2017-06-04 21:53:14 +0900

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+
+  host = 'localhost:3000'                     # ローカル環境
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
