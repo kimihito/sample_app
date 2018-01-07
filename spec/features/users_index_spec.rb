79785a8a0cb02366e3d7e878edcb00b72fcc3758
kimihito <tatsurotamashiro@gmail.com>
2018-01-07 21:56:01 +0900

    fixup! Add users_index_spec

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Edit user', js: true do
   fixtures :users
-
   let!(:admin) { users(:michael) }
-  let!(:no_admin) { users(:archer) }
+  let!(:non_admin) { users(:archer) }
 
   describe 'index as admin includeing pagination and delete links' do
     before { login_as(admin) }
@@ -19,8 +18,18 @@ RSpec.describe 'Edit user', js: true do
     it 'should delete non admin user' do
       visit users_path
       expect {
-        find_link(text: 'delete', href: user_path(no_admin)).click
-      }.to change { User.count }.by(-1)
+        page.accept_confirm do
+          find_link(text: 'delete', href: user_path(non_admin)).click
+        end
+      }.to change { User.all.size }.by(-1)
+    end
+  end
+
+  describe 'index as non-admin' do
+    before { login_as(non_admin) }
+    it 'should not have delete link' do
+      visit users_path
+      expect(page).to have_no_link('delete')
     end
   end
 end
